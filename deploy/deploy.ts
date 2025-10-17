eb49ba2b32a99bce6608e054a969566a053bbd47
gideonhu <igtsyrt3834@hotmail.com>
2025-10-17 16:06:21 +0800

    deploy scipt

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,5 +1,7 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
@@ -11,7 +13,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   console.log(`FHECounter contract: `, deployedFHECounter.address);
+
+  // Deploy ConfidentialUSDT (cUSDT)
+  const deployedCUSDT = await deploy("ConfidentialUSDT", {
+    from: deployer,
+    log: true,
+  });
+  console.log(`ConfidentialUSDT (cUSDT) contract: `, deployedCUSDT.address);
+
+  // Deploy CUSDTStaking with token address
+  const deployedStaking = await deploy("CUSDTStaking", {
+    from: deployer,
+    args: [deployedCUSDT.address],
+    log: true,
+  });
+  console.log(`CUSDTStaking contract: `, deployedStaking.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_all"; // id required to prevent reexecution
+func.tags = ["FHECounter", "ConfidentialUSDT", "CUSDTStaking"];
